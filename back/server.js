0695e4801ff2b918b26e97c511bd91a88fa89fc6
manya garg <manyagarg787@gmail.com>
2023-12-20 00:16:33 +0530

    done

---
+++ back/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/User');
+const examQuestionsRoute = require('./routes/ExamQuestions');
+const userExamsRoute = require('./routes/UserExams');
+const examRoute = require('./routes/Exam');
+const connectToDatabase = require('./db');
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+connectToDatabase();
+
+app.use('/users', userRoute);
+app.use('/examquestions', examQuestionsRoute);
+app.use('/exam', examRoute);
+app.use('/userexams', userExamsRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on ${process.env.PORT}`);
+});
